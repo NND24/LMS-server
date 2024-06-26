789ad6bca0a8e58f4f7eae8f54dd867494f06869
Dat Nguyen <=>
2024-06-26 19:37:10 +0700

    feat: Add handle errors

--- app.ts
+++ app.ts
@@ -1,2 +1,25 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middlewares/error";
+
 export const app = express();
+
+require("dotenv").config();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// Unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(ErrorMiddleware);
