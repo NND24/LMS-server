4bf69d87223cbf81fc8a0c4390a01b9c2b083a4f
Dat Nguyen <=>
2024-08-05 21:38:54 +0700

    Update origin

--- build/app.js
+++ build/app.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 exports.app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000"],
+    origin: ["https://e-learning-red.vercel.app"],
     credentials: true,
 }));
 // Routes
